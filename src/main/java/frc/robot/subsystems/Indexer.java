37f372f3bab55aa2b942b7900a5bf0d86a088dde
arnavrthota <arnavthota@outlook.com>
2024-11-14 19:21:01 -0800

    Indexer Code

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,5 +1,24 @@
 package frc.robot.subsystems;
 
-public class Indexer {
+import com.ctre.phoenix6.controls.Follower;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import pabeles.concurrency.ConcurrencyOps.NewInstance;
+
+
+
+public class Indexer extends SubsystemBase{
+    private static Indexer instance;
+    public static Indexer getIndexer() {
+        if (instance == null){
+            instance = new Indexer();
+        }
+        return instance;
+    }
     
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,15 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.Follower;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class Shooter {
-    //test
+    
 }
