53dcd821ff7b76653a0ec0144ce59d2f83a92717
simplylunar <ashank.mehrotra@hotmail.com>
2024-11-14 20:06:09 -0800

    worked on shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,28 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter {
     
+    private static Shooter instance;
+
+    public static Shooter getInstance(){
+        if (instance == null) {
+            instance = new Shooter();
+        }
+        return instance;
+    }
+    private TalonFX shooterTopM;
+    private TalonFX shooterBottomM;
+    
+
 }
