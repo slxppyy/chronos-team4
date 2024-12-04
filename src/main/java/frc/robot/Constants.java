0af78a967841339ad813e260b3fab6635bd1df8b
arnavrthota <arnavthota@outlook.com>
2024-12-03 19:23:59 -0800

    added constants file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,24 +1,29 @@
 package frc.robot;
 
 public final class Constants {
-    public static final class HardwarePorts {
-        public static final int shooterBottomM = 50;
-        public static final int shooterTopM = 51;
-        
-        public static final int indexerTopM = 31;
-        public static final int indexerBottomM = 30;
+    public static final int frontLeftSteer = 1;
+    public static final int frontLeftDrive = 2;
 
-        public static final int intakeLeaderM = 21;
-        public static final int intakeFollower = 20;
-        public static final int serialM = 22;
+    public static final int backLeftSteer = 3;
+    public static final int backLeftDrive = 4;
 
-        public static final int climbLeaderMotor = 60;
-        public static final int climbFollowerMotor = 61;
+    public static final int backRightSteer = 5;
+    public static final int backRightDrive = 6;
 
-        public static final int pivotLeaderM = 40;
-        public static final int pivotFollowerM = 41;
-        public static final int pivotCANcoderID = 42;
+    public static final int frontRightSteer = 7;
+    public static final int frontRightDrive = 8;
 
-        public static final int ampM = 62;
-    }
+    public static final int frontLeftCancoder = 9;
+    public static final int backLeftCancoder = 10;
+    public static final int backRightCancoder = 11;
+    public static final int frontRightCancoder = 12;
+
+
+    public static final int intake = 40;
+
+    public static final int leftIndexer = 21;
+    public static final int rightIndexer = 22;
+
+    public static final int leftShooter = 31;
+    public static final int rightShooter = 32;
 }
--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+
+//import com.revrobotics.ColorSensorV3;
+
+
 import com.ctre.phoenix6.controls.Follower;
 
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -8,29 +12,23 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-<<<<<<< Updated upstream
-import pabeles.concurrency.ConcurrencyOps.NewInstance;
-
-=======
-import frc.robot.*;
-import com.ctre.phoenix6.controls.Follower;
-import frc.robot.ShooterConstants;
->>>>>>> Stashed changes
+import frc.robot.Constants;
 
 
 public class Indexer extends SubsystemBase{
     private static Indexer instance;
-    public static Indexer getIndexer() {
-        if (instance == null){
+    public static Indexer getInstance() {
+        if (instance == null) {
             instance = new Indexer();
         }
         return instance;
     }
-    
-<<<<<<< Updated upstream
-=======
-    private Follower follow = new Follower(ShooterConstants.HardwarePorts.indexerTopM, false );
-    
 
->>>>>>> Stashed changes
+    private TalonFX indexerLeaderM;
+    private TalonFX indexerFollowerM;
+    
+    private Follower follow = new Follower(Constants.rightIndexer, false);
+    
+    //private ColorSensorV3 colorSensor;
+
 }
