73761d5727d3b837e3604265155818d1bb457e8b
arnavrthota <arnavthota@outlook.com>
2024-12-03 18:42:23 -0800

    Update Indexer.java

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Indexer {
-    
+
+public class Indexer extends SubsystemBase{
+    private static Indexer instance;
+    public static Indexer getInstance() {
+        if (instance == null) {
+            instance = new Indexer();
+        }
+        return instance;
+    }
+
+    private TalonFX indexerLeaderM;
+    private TalonFX indexerFollowerM;
+
+    //private Follower follow = new Follower(Constants.HardwarePorts.indexerTopM, false);
 }
