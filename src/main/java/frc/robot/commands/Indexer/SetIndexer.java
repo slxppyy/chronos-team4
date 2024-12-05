8ffa02c0afdbc85564c2580e0312fc338b952a2e
arnavrthota <arnavthota@outlook.com>
2024-12-04 16:53:41 -0800

    Update SetIndexer.java

--- src/main/java/frc/robot/commands/Indexer/SetIndexer.java
+++ src/main/java/frc/robot/commands/Indexer/SetIndexer.java
@@ -1,5 +1,23 @@
 package frc.robot.commands.Indexer;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Indexer.IndexerStates;
+public class SetIndexer extends Command{
+    private final Indexer s_Indexer;
+    IndexerStates state;
+    private final boolean intaking;
+    private double time;
+    private Timer timer;
 
-public class SetIndexer {
+    public SetIndexer(IndexerStates state, boolean intaking, double time) {
+        this.intaking = intaking;
+        s_Indexer = Indexer.getInstance();
+        this.state = state;
+        timer = new Timer();
+        addRequirements(s_Indexer);
+        this.time = time;
+    }
+    
     
 }
