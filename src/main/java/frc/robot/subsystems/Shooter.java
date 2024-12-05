00dd0565e051ef2f538e8c3be44e9c946fc1bd52
simplylunar <ashank.mehrotra@hotmail.com>
2024-12-04 20:40:38 -0800

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,16 @@
 package frc.robot.subsystems;
 
+<<<<<<< Updated upstream
 import com.ctre.phoenix6.controls.Follower;
+=======
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+  import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+>>>>>>> Stashed changes
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
@@ -12,4 +22,30 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter {
     
+<<<<<<< Updated upstream
+=======
+    private static volatile Shooter instance;
+//THREAD SAFE INSTANCE GETTER SEARCH ON YT 
+    public static Shooter getInstance(){
+        if (instance == null) {
+            synchronized(Shooter.class) {
+                if (instance == null) {
+                    instance = new Shooter();
+                }
+            }
+        }
+        return instance;
+    }
+    private TalonFX shooterTopM;
+    private TalonFX shooterBottomM;
+
+    private double topVelocitySetpoint = 0;
+    private double botVelocitySetpoint = 0;
+
+    final VelocityVoltage topVelocityVoltage = new VelocityVoltage(0);
+    final VelocityVoltage 
+
+
+
+>>>>>>> Stashed changes
 }
