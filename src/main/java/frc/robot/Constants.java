67c3e01442741b2f0e50a119ebd8f959e9ec9850
dustyawsome7 <abbas.jum53@gmail.com>
2024-12-05 18:32:47 -0800

    Part 2 update

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,6 @@
+package frc.robot;
+
+public final class Constants {
+    public static final int intakePeakCurrentLimit = 70;
+    public static final int intakeContinuousCurrentLimit = 30;
+}
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,7 +10,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.Constants;
+import frc.robot.Constants;
 
 public class Intake {
     private static Intake instance;
@@ -25,8 +25,28 @@ public class Intake {
     
 }
 
+private TalonFX intakeMotor;
 
-private void configMotor() {}
+private void configMotor(TalonFX motor) {
+
+
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    CurrentLimitsConfigs currentLimitsConfigs = new CurrentLimitsConfigs();
+
+    currentLimitsConfigs.SupplyCurrentLimit = Constants.intakeContinuousCurrentLimit;
+    currentLimitsConfigs.SupplyCurrentLimitEnable = true;
+    currentLimitsConfigs.SupplyCurrentThreshold = Constants.intakePeakCurrentLimit;
+    config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    config.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.5;
+    //config.OpenLoopRamps.
+
+    //Slot0Configs slot0Configs = new Slot0Configs();
+    //slot0Configs.kP = Constants.SwerveConstants.driveKP;
+    //slot0Configs.kI = Constants.SwerveConstants.driveKI;
+    //slot0Configs.kD = Constants.SwerveConstants.driveKD;
+    config.CurrentLimits = currentLimitsConfigs;
+    motor.getConfigurator().apply(config);
+}
 
 
 public enum IntakeStates {}
