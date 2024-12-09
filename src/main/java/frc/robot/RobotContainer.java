6000d7797a35897ef296bfeb408f27fa4e07aef1
arnavrthota <arnavthota@outlook.com>
2024-12-08 19:26:49 -0800

    command factory

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -44,10 +44,32 @@ public class RobotContainer {
 
   public static final double translationDeadband = 0.1;
   public static final double rotDeadband = 0.1;
-}
+
+  
+  private final Trigger driverBack = driver.back();
+  private final Trigger driverStart = driver.start();
+  private final Trigger driverA = driver.a();
+  private final Trigger driverB = driver.b();
+  private final Trigger driverX = driver.x();
+  private final Trigger driverY = driver.y();
+  private final Trigger driverRightBumper = driver.rightBumper();
+  private final Trigger driverLeftBumper = driver.rightBumper();
+  private final Trigger driverLeftTrigger = driver.leftTrigger();
+  private final Trigger driverRightTrigger = driver.rightTrigger();
+  private final Trigger driverDpadUp = driver.povUp();
+  private final Trigger driverDpadDown = driver.povDown();
+  private final Trigger driverDpadLeft = driver.povLeft();
+  private final Trigger driverDpadRight = driver.povRight();
+
+  public CommandXboxController getDriverController(){
+    return driver;
+  }
 
   private void configureBindings() {
-
+    //driver.a().onTrue(CommandFactory.offEverything());
+    //driver.x().onTrue(new SmartInake());
+    //driver.b().onTrue(CommandFactory.eject());
+    //driver.y().whileTrue(new SetIndexer(IndexerStates.SHOOTING))
 
   }
 
