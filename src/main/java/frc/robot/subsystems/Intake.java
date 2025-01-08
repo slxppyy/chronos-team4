8ef803d138bd1ae2efd20a3e3eb2ae6352883cbc
arnavrthota <arnavthota@outlook.com>
2025-01-07 19:05:35 -0800

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,15 +4,14 @@ import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.Constants;
+import frc.robot.Constants;
 
-public class Intake {
+public class Intake extends SubsystemBase{
     private static Intake instance;
     public static Intake getInstance() {
         if (instance == null) {
@@ -23,20 +22,67 @@ public class Intake {
         return instance;
     }
     
-}
 
-
-private void configMotor() {}
-
-
-public enum IntakeStates {}
-
-public double getIntakeVoltage() {
-
-}
-
-public void setIntakeSpeed() {
+private DutyCycleOut dutyCycleRequest = new DutyCycleOut(0);
+private IntakeStates currentState = IntakeStates.OFF;
     
-}
+    
 
+private TalonFX intakeMotor;
+public Intake(){
+    intakeMotor = new TalonFX(Constants.intake);
+    configMotor(intakeMotor);
+    intakeMotor.setInverted(false);
 }
+private void configMotor(TalonFX motor) {
+
+
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    CurrentLimitsConfigs currentLimitsConfigs = new CurrentLimitsConfigs();
+
+    currentLimitsConfigs.SupplyCurrentLimit = Constants.intakeContinuousCurrentLimit;
+    currentLimitsConfigs.SupplyCurrentLimitEnable = true;
+    currentLimitsConfigs.SupplyCurrentThreshold = Constants.intakePeakCurrentLimit;
+    config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    config.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.5;
+    //config.OpenLoopRamps.
+
+    //Slot0Configs slot0Configs = new Slot0Configs();
+    //slot0Configs.kP = Constants.SwerveConstants.driveKP;
+    //slot0Configs.kI = Constants.SwerveConstants.driveKI;
+    //slot0Configs.kD = Constants.SwerveConstants.driveKD;
+    config.CurrentLimits = currentLimitsConfigs;
+    motor.getConfigurator().apply(config);
+    }
+
+
+    public enum IntakeStates {
+    ON(0.65, 0.85),
+    OFF(0, 0),
+    REV(-0.4, -0.8);
+
+    private double speed;
+    private double serialSpeed;
+
+        public double getValue() {
+            return speed;
+        }
+
+        IntakeStates(double speed, double serialSpeed) {
+            this.speed = speed;
+            this.serialSpeed = serialSpeed;
+        }
+
+    }
+
+
+    public void setSpeed(double percentageOutput) {
+        intakeMotor.set(percentageOutput);
+    }
+
+
+
+}  
+    
+
+    
