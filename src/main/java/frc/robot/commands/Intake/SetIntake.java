a1b7ea151619d911533c153fbaec2413b6302949
arnavrthota <arnavthota@outlook.com>
2025-01-07 18:59:19 -0800

    Update SetIntake.java

--- src/main/java/frc/robot/commands/Intake/SetIntake.java
+++ src/main/java/frc/robot/commands/Intake/SetIntake.java
@@ -1,5 +1,40 @@
 package frc.robot.commands.Intake;
 
-public class SetIntake {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Intake.IntakeStates;
+
+public class SetIntake extends Command {
+    private final Intake s_Intake;
+    Intake.IntakeStates state;
+
+   
+    // Overloaded constructor
+    public SetIntake(Intake.IntakeStates state) {
+        s_Intake = Intake.getInstance();
+        addRequirements(s_Intake);
+        this.state = state; // Check later 
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println("INTAKE");
+        s_Intake.setSpeed(state.getValue());
+    }
     
+    @Override
+    public void execute() {
+
+    
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() { 
+        return true;
+    }
 }
